422db60897c9a680731da37b0c0532f7f784ad26
Kei-K23 <keiksl2301@gmail.com>
2024-05-20 20:31:17 +0630

    setup

---
+++ src/main/java/dev/kei/App.java
@@ -0,0 +1,12 @@
+package dev.kei;
+
+/**
+ * calc CLI also a parser that can perform mathematical calculations and written in Java.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(args[0]);
+    }
+}
